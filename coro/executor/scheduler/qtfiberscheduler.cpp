361e5647c4725c8b4cc568dee3aa816872285da9
David Chen <ste7ann@gmail.com>
2026-07-21 02:10:40 -0700

    fix(scheduler): start Qt pump before first wait

--- coro/executor/scheduler/qtfiberscheduler.cpp
+++ coro/executor/scheduler/qtfiberscheduler.cpp
@@ -13,7 +13,9 @@ Coro::QtFiberScheduler::~QtFiberScheduler(void)
 
 void Coro::QtFiberScheduler::suspend_until(const std::chrono::steady_clock::time_point &time_point) noexcept
 {
-    std::call_once(pump_once_, [this]{
+    bool startedPump = false;
+    std::call_once(pump_once_, [this, &startedPump]{
+        startedPump = true;
         boost::fibers::fiber(launch_properties([this]{
             while (!FiberScheduler::s_exit_.load(std::memory_order_acquire)
                    && !FiberScheduler::t_stop_.load(std::memory_order_acquire)) {
@@ -22,5 +24,6 @@ void Coro::QtFiberScheduler::suspend_until(const std::chrono::steady_clock::time
             }
         }, Priority::High, Affinity::fixed(std::this_thread::get_id()))).detach();
     });
+    if(startedPump) return;
     FiberScheduler::suspend_until(time_point);
 }
